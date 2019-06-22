e9a97d8bcec76f650fb279857884e24364d3972a
Ramya Priyadharshini <ramya@Ramyas-MacBook-Pro.local>
2019-06-22 23:15:52 +0530

    one hot encode

--- transfer_learning_experiment/from_one_art_to_another/transfer_learning_experiment.py
+++ transfer_learning_experiment/from_one_art_to_another/transfer_learning_experiment.py
@@ -47,17 +47,10 @@ class ExperimentHandler(object):
     def one_hot_encoding(self, total_labels):
         one_hot_encodings = list()
         encoder = LabelEncoder()
-
-        for label in total_labels:
-            self.n_labels = len(Counter(label).keys())
-
-            encoder.fit(label)
-            encoded_y = encoder.transform(label)
-
-            final_y = np_utils.to_categorical(encoded_y, self.n_labels)
-
-            one_hot_encodings.append(final_y)
-
+        self.n_labels = len(Counter(total_labels).keys())
+        encoded_y = encoder.fit_transform(total_labels)
+        one_hot_encodings = np_utils.to_categorical(encoded_y, self.n_labels)
+        print (one_hot_encodings)
         return(one_hot_encodings)
 
     def store_images_to_hdf5(self, path, images, split):
@@ -77,7 +70,7 @@ class ExperimentHandler(object):
         f = h5py.File(path)
         dset = f.create_dataset(split, data=encodings)
 
-    def make_data_splits(self, images, one_hot_encodings):
+    def make_data_splits(self, images, labels):
         if not os.path.exists(self.dataset_storing_path+"/"):
             os.makedirs(self.dataset_storing_path+"/")
 
@@ -94,21 +87,20 @@ class ExperimentHandler(object):
             testing_images_path = self.dataset_storing_path+"/"+"testing_images.hdf5"
             testing_labels_path = self.dataset_storing_path+"/"+"testing_labels.hdf5"
 
-            for labels in one_hot_encodings:
-                X_train, X_val, y_train, y_val = train_test_split(images, labels, test_size=0.2, random_state=42)
+            X_train, X_val, y_train, y_val = train_test_split(images, labels, test_size=0.2, random_state=42)
 
-                self.store_images_to_hdf5(training_images_path, X_train, 'X_train')
-                self.store_encodings_to_hdf5(training_labels_path, y_train, 'y_train')
+            self.store_images_to_hdf5(training_images_path, X_train, 'X_train')
+            self.store_encodings_to_hdf5(training_labels_path, y_train, 'y_train')
 
-                X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.5 , random_state=42)
+            X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.5 , random_state=42)
 
-                self.store_images_to_hdf5(validation_images_path, X_val, 'X_val')
-                self.store_encodings_to_hdf5(validation_labels_path, y_val, 'y_val')
+            self.store_images_to_hdf5(validation_images_path, X_val, 'X_val')
+            self.store_encodings_to_hdf5(validation_labels_path, y_val, 'y_val')
 
-                self.store_images_to_hdf5(testing_images_path, X_test, 'X_test')
-                self.store_encodings_to_hdf5(testing_labels_path, y_test, 'y_test')
+            self.store_images_to_hdf5(testing_images_path, X_test, 'X_test')
+            self.store_encodings_to_hdf5(testing_labels_path, y_test, 'y_test')
 
-                print("The splits have been created!")
+            print("The splits have been created!")
         else:
             print("The splits are already there!")
             self.hdf5_path = os.path.dirname(self.dataset_storing_path+"/")
