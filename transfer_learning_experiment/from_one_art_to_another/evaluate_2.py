4661b436c2337961d99cea925978802ce3373eca
Ramya Priyadharshini <ramya@192.168.1.6>
2019-06-24 19:59:24 +0530

    added evaluate_2

--- transfer_learning_experiment/from_one_art_to_another/evaluate_2.py
+++ transfer_learning_experiment/from_one_art_to_another/evaluate_2.py
@@ -1,87 +1,55 @@
-# coding: utf-8
-from imagenet_vector import get_imagenet_vector
 from keras.models import load_model
-m = load_model('/media/dev_hdd2/ramya/art_style_exps/approaches/iter2/results/Art_Style_Macys/from_one_art_to_another/VGG19/TL_VGG19_model.h5')
-m.load_weights('/media/dev_hdd2/ramya/art_style_exps/approaches/iter2/results/Art_Style_Macys/from_one_art_to_another/VGG19/TL_VGG19_weights.h5')
-df = pd.read_csv('/media/train_hdd2/Macys_phase3/art_testset/art_style_testset_inp_valid.csv')
-df.iloc[0]
 import pandas as pd
-df = pd.read_csv('/media/train_hdd2/Macys_phase3/art_testset/art_style_testset_inp_valid.csv')
-df.iloc[0]
-df.labels.value_counts()
-df = pd.read_csv('/media/train_hdd2/Macys_phase3/art_testset/art_style_testset_inp_valid_reduced_7cls_cropped_images.csv')
-df.head()
-k = []
-for idx, row in df.iterrows():
-    img = image.load_img(row['img_path'], target_size=(size,size))
-    img = image.img_to_array(img)
-    img = preprocess_input(np.expand_dims(img, axis=0))
-    pred = m.predict(img)
-    res = classes[np.argmax(pred)]
-    pred = pred[0].tolist()
-    df.loc[idx, 'predictions'] = str([round(i,3) for i in pred])
-    df.loc[idx, 'pred'] = res
-    print (row['img_path'])
-    
 from keras.preprocessing import image
 from keras.applications.vgg19 import preprocess_input
-k = []
-for idx, row in df.iterrows():
-    img = image.load_img(row['img_path'], target_size=(size,size))
-    img = image.img_to_array(img)
-    img = preprocess_input(np.expand_dims(img, axis=0))
-    pred = m.predict(img)
-    res = classes[np.argmax(pred)]
-    pred = pred[0].tolist()
-    df.loc[idx, 'predictions'] = str([round(i,3) for i in pred])
-    df.loc[idx, 'pred'] = res
-    print (row['img_path'])
-    
+import sys
+import os
+import numpy as np
+from sklearn.metrics import accuracy_score, classification_report
+from pandas_ml import ConfusionMatrix
+import matplotlib.pyplot as plt
+
+# Config
 size = 224
 channels = 3
-k = []
-for idx, row in df.iterrows():
-    img = image.load_img(row['img_path'], target_size=(size,size))
-    img = image.img_to_array(img)
-    img = preprocess_input(np.expand_dims(img, axis=0))
-    pred = m.predict(img)
-    res = classes[np.argmax(pred)]
-    pred = pred[0].tolist()
-    df.loc[idx, 'predictions'] = str([round(i,3) for i in pred])
-    df.loc[idx, 'pred'] = res
-    print (row['img_path'])
-    
-import pandas as pd
-import numpy as np
-k = []
-for idx, row in df.iterrows():
-    img = image.load_img(row['img_path'], target_size=(size,size))
-    img = image.img_to_array(img)
-    img = preprocess_input(np.expand_dims(img, axis=0))
-    pred = m.predict(img)
-    res = classes[np.argmax(pred)]
-    pred = pred[0].tolist()
-    df.loc[idx, 'predictions'] = str([round(i,3) for i in pred])
-    df.loc[idx, 'pred'] = res
-    print (row['img_path'])
-    
+
+model_path = sys.argv[1]
+weights_path = sys.argv[2]
+inp_csv = sys.argv[3]
+op_csv = sys.argv[4]
+
+# Load model
+m = load_model()
+m.load_weights()
+
+df = pd.read_csv(inp_csv)
 classes = sorted(pd.unique(df.labels))
+
+
 k = []
 for idx, row in df.iterrows():
-    img = image.load_img(row['img_path'], target_size=(size,size))
+    img = image.load_img(row['img_path'], target_size=(size, size))
     img = image.img_to_array(img)
     img = preprocess_input(np.expand_dims(img, axis=0))
     pred = m.predict(img)
     res = classes[np.argmax(pred)]
     pred = pred[0].tolist()
-    df.loc[idx, 'predictions'] = str([round(i,3) for i in pred])
+    df.loc[idx, 'predictions'] = str([round(i, 3) for i in pred])
     df.loc[idx, 'pred'] = res
-    print (row['img_path'])
-    
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-accuracy_score(df['labels'].tolist(), df['pred'].tolist())
-df.shape
-print(classification_report(df['labels'].tolist(), df['pred'].tolist(), target_names=classes))
-df.to_csv('/media/train_hdd2/Macys_phase3/art_testset/art_style_testset_inp_valid_reduced_7cls_cropped_images_output_best_model_v2.csv', index=False)
-from pandas_ml import ConfusionMatrix
-ConfusionMatrix(df['labels'].tolist(), df['pred'].tolist())
+
+df.to_csv(op_csv, index=False)
+
+# Metrics
+print (accuracy_score(df['labels'].tolist(), df['pred'].tolist()))
+print (classification_report(df['labels'].tolist(), df['pred'].tolist(), target_names=classes))
+cm = ConfusionMatrix(df['labels'].tolist(), df['pred'].tolist()).to_dataframe()
+
+# Save metrics
+cm.to_csv(os.path.join(os.path.dirname(op_csv), 'confusion_matrix.csv'), index=False)
+fig = plt.figure()
+plt.matshow(cm)
+plt.title('Confusion Matrix')
+plt.colorbar()
+plt.ylabel('True Label')
+plt.xlabel('Predicted Label')
+plt.savefig(os.path.join(os.path.dirname(op_csv), 'confusion_matrix.jpg'))
